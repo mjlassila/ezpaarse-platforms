92db0ce4f5dbd1a23f9cf7546498e7c09bc0f36b
Matti Lassila <matti.lassila@gmail.com>
2022-01-10 17:06:04 +0200

    Initial parser for Alma Talent Suomen Laki subsection

--- almatalent/parser.js
+++ almatalent/parser.js
@@ -20,12 +20,13 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // console.error(parsedUrl);
 
   let match;
+  let category;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if ((match = /^\/teos\/([A-Z0-9]*)$/i.exec(path)) !== null) {
+    //  https://bisneskirjasto.almatalent.fi:443/teos/BAEBBXXTBBAEE
+    //  https://verkkokirjahylly.almatalent.fi:443/teos/DAEBIXCTEB
+    result.rtype    = 'BOOK';
+    result.mime     = 'HTML';
 
     /**
      * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
@@ -33,20 +34,32 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
      * it can be a DOI, an internal identifier or a part of the accessed URL
      * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
      */
-    result.unitid = match[2];
+    result.unitid = match[1];
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+  } else if ((match = /^\/article\/(.*)\/(\d*)$/i.exec(path)) !== null) {
+    // https://pro.almatalent.fi:443/article/terrorismirikoslainsaadantoon-muutoksia/19143
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[2];
+  } else if ((match = /^\/TimePub\/DatabaseCSS\.ashx$/i.exec(path)) !== null) {
+    // https://suomenlaki.almatalent.fi:443/TimePub/DatabaseCSS.ashx?category=Book&docType=557231&documentId=18as419836&timeMachineDate=null&containerClass=article&blockClassPrefix=bsI&paragraphClassPrefix=bsP&MMName=TimePub.css
+    // https://suomenlaki.almatalent.fi:443/TimePub/DatabaseCSS.ashx?category=Regulation&docType=Regulation&documentId=Ym401&timeMachineDate=2020-03-10&containerClass=article&blockClassPrefix=bsI&paragraphClassPrefix=bsP&MMName=TimePub.css
+    result.mime     = 'HTML';
+    result.unitid   = param.documentId;
+    category = param.category;
+    if (category === 'Book') {
+      result.rtype = 'BOOK';
+    }
+    else if (category === 'Regulation') {
+      result.rtype = 'ARTICLE';
+    }
   }
 
   if (hostname === 'verkkokirjahylly.almatalent.fi') {
-    result.publication_title = 'Verkkokirjahylly';
+    result.publication_title = 'Alma Talent Verkkokirjahylly';
   } else if (hostname === 'bisneskirjasto.almatalent.fi') {
-    result.publication_title = 'Bisneskirjasto';
+    result.publication_title = 'Alma Talent Bisneskirjasto';
   } else if (hostname === 'pro.almatalent.fi') {
     result.publication_title = 'Alma Talent Pro';
   } else if (hostname === 'suomenlaki.almatalent.fi') {
