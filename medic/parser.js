44961668a2d47034ec4a7083633b786f010315c6
Matti Lassila <matti.lassila@gmail.com>
2022-01-11 10:39:12 +0200

    Initial parser for Medic

--- medic/parser.js
+++ medic/parser.js
@@ -14,18 +14,18 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if ((match = /^\/medic\/$/i.exec(path) && param.hakusanat) !== null) {
+    // http://www.terkko.helsinki.fi:80/medic/?hakusanat=epilep&sivu=haku&mista=&alkaen=0&ratio=AND&hakusanat2=epilepsia+epileps*&mista2=&ratio2=AND&hakusanat3=&mista3=&vuosi1=&vuosi2=&synonyymit=true&kieli%5B%5D=kaikki&tyyppi%5B%5D=kaikki
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.platform_name = 'Medic'
 
     /**
      * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
@@ -33,15 +33,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
      * it can be a DOI, an internal identifier or a part of the accessed URL
      * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
      */
-    result.unitid = match[2];
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
-  }
+    let keywords = [param.hakusanat,param.hakusanat2,param.hakusanat3];
+    result.unitid = keywords.filter(x => typeof x === 'string' && x.length > 0).join("+");
+  
+
+  } 
 
   return result;
 });
