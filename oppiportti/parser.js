ac9a13ad3b13295d2414ea4e83f9f33446f83147
Matti Lassila <matti.lassila@gmail.com>
2022-01-18 11:37:15 +0200

    Collected URLs for subsections of Oppiportti

--- oppiportti/parser.js
+++ oppiportti/parser.js
@@ -22,9 +22,18 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let match;
 
   if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+    // https://www.oppiportti.fi/op/kbk00170/do (oppikirjan sisältö, do-lopussa)
+    // https://www.oppiportti.fi/op/opk04637 (oppikirjat esittelysivu)
+    // https://www.oppiportti.fi/op/lko00015 (laitekoulutukset)
+    // https://www.oppiportti.fi/op/owb00016 (webinaarit)
+    // https://www.oppiportti.fi/op/olk00012 (luennot)
+    // https://www.oppiportti.fi/op/vdu00019 (videot)
+    // https://www.oppiportti.fi/op/ott00013 (testit)
+    // https://www.oppiportti.fi/op/dvk00191 (verkkokurssit) https://www.oppiportti.fi/op/dvk00191/avaa
+    // https://www.oppiportti.fi/op/ovr00003 (virtuaaliharjoitukset)
+    // muissa paitsi kirjoissa lopussa avaa tarkoittaa sisällön avaamista
     result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
+    result.mime     = 'HTML';
     result.title_id = match[1];
 
     /**
@@ -36,8 +45,8 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.unitid = match[2];
 
   } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+    // https://www.oppiportti.fi/op/xhakutulos?p_haku=farmakologia
+    result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[2];
