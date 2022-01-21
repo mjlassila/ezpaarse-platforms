4d21dc4650f33b5c06797218eee9ca9d94c78cd9
Matti Lassila <matti.lassila@gmail.com>
2022-01-21 13:07:57 +0200

    Initial parser for Sähköinfo Severi subsections

--- sahkoinfo/parser.js
+++ sahkoinfo/parser.js
@@ -14,33 +14,46 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+  if ((match = /^\/item\/([0-9]+)$/i.exec(path)) !== null) {
+    // https://severi.sahkoinfo.fi:443/pdfget/5456 (tällä latautuu kaikki PDF-tyyppiset tiedostot)
+    // https://severi.sahkoinfo.fi:443/Search/PerformSearch
+    // https://severi-sahkoinfo-fi.libproxy.tuni.fi/Magazine/Open/2773 (lehti tai lehden artikkeli)
+    // https://severi.sahkoinfo.fi/item/2159?search=pistorasiat
+    // https://severi.sahkoinfo.fi:443/Browse/JsonProductList?id=6
+    // https://severi.sahkoinfo.fi:443/Content/Index/0?newsid=498
+    // https://severi.sahkoinfo.fi:443/Browse/MetaItems_Read/905
+
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = match[1];
+    result.unitid   = 'item/' +  match[1];
+    result.search_term = param.search;
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+  } else if ((match = /^\/Search\/PerformSearch$/i.exec(path)) !== null) {
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/Magazine\/Open\/([0-9]+)$/i.exec(path)) !== null) {
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid = match[1];
+  } else if ((match = /^\/Content\/Index\/([0-9]+)$/i.exec(path)) !== null) {
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid = 'news/' + param.newsid;
+  } else if ((match = /^\/Browse\/JsonProductList$/i.exec(path)) !== null) {
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.unitid   = param.id;
+  } else if ((match = /^\/Browse\/MetaItems_Read\/([0-9]+)$/i.exec(path)) !== null) {
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
   }
 
   return result;
