d96d039a2f0dfe5b6cd18fa968be3165462417a2
Matti Lassila <matti.lassila@gmail.com>
2022-01-24 15:37:04 +0200

    Parsers for subsections of Lääkärilehti

--- laakari/parser.js
+++ laakari/parser.js
@@ -13,19 +13,18 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+  if ((match = /^\/pdf\/([0-9]{4})\/([A-Za-z0-9-])\.pdf$/i.exec(path)) !== null) {
+    // https://www.laakarilehti.fi/pdf/2021/SLL502021-3010.pdf
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = match[1];
+    result.unitid = match[1] + '/' + match[2];
 
     /**
      * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
@@ -35,13 +34,37 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
      */
     result.unitid = match[2];
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+  } else if ((match = /^\/erikoisalat\/([0-9]+)$/i.exec(path)) !== null) {
+    // https://www.laakarilehti.fi/erikoisalat/807
+    result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid   = 'erikoisalat/' + match[1] ;
+  } else if ((match = /^\/sisallysluettelo/i.exec(path)) !== null) {
+    // https://www.laakarilehti.fi/sisallysluettelo/?year=1992&magazine=15601
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.unitid   = param.year + '/' + param.magazine;
+  } else if ((match = /^\/haku/i.exec(path)) !== null) {
+    // https://www.laakarilehti.fi/sisallysluettelo/?year=1992&magazine=15601
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.search_term   = [param.q, param.keywords].join(' ');
   }
 
   return result;
 });
+
+// https://www.laakarilehti.fi/tyossa/kantapaan-kautta/painehaavoja-ei-pitaisi-syntya/
+// https://www.laakarilehti.fi/tieteessa/katsausartikkeli/nayttoon-perustuva-hoito-isanta-vai-renki/
+// https://www.laakarilehti.fi/kliininen-tyo/terveyskeskusten-laakarivaje-on-pysynyt-ennallaan/
+// https://www.laakarilehti.fi/tieteessa/terveydenhuoltoartikkelit/aktiivisen-hoidon-tulee-jatkua-paivystysleikkauksen-jonotusaikanakin/
+// https://www.laakarilehti.fi/terveydenhuolto/laakariliitto-terveydenhuollon-rahoitusta-nostettava/
+// https://www.laakarilehti.fi/pdf/2021/SLL502021-3010.pdf
+// https://www.laakarilehti.fi/sisallysluettelo/?year=1992&magazine=15601
+// https://www.laakarilehti.fi/svenska/ledare/20.8.1992-hucs-delegationen/
+// https://www.laakarilehti.fi/arkisto/muut-laakarilehdet/15669.1039.164_tuberkuloosi-pelottaa-jo-hoitohenkilokuntaakin/
+// https://www.laakarilehti.fi/arkisto/octopus/minimaalinen-muistiaukko/
+// https://www.laakarilehti.fi/arkisto/paakirjoitukset/10.7.1992-kunnallinen-paasopimus-hylattiin/
+// https://www.laakarilehti.fi/haku/?keywords=&author=&title=&year_min=1992&year_max=2022&magazine_issue=&page_number=&limit=10&advanced=1&q=alaraaja&sort=new_first
+// https://www.laakarilehti.fi/erikoisalat/807
+// https://www.laakarilehti.fi/mielipide/tuliko-helpotusta-laakarivajeeseen/
