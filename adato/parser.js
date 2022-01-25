a80d10ca9b6b38b9d72aeefa228bf38b3adc037a
Matti Lassila <matti.lassila@gmail.com>
2022-01-25 14:23:25 +0200

    Parsers for Sähköverkkoekstra subsections

--- adato/parser.js
+++ adato/parser.js
@@ -14,34 +14,43 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+  if ((match = /^\/system\/files\/filedepot\/[0-9a-z]+\/([a-zA-Z0-9_-]+\.[a-z]+)$/i.exec(path)) !== null) {
+    // https://sahkoverkkoekstra.fi/system/files/filedepot/7/rm_3-16_kaap-mmo.pdf
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = match[1];
+    result.unitid = match[1];
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+  } else if ((match = /^\/file\/([0-9]+)\/download$/i.exec(path)) !== null) {
+    // https://sahkoverkkoekstra.fi/file/575/download?token=WGJBkPFo
+    // http://sahkoverkkoekstra.fi/file/152/download?token=rhNOgnYr
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = match[1] + '/' + param.token;
+  } else if ((match = /^\/filedepot_download\/([0-9]+)\/([0-9]+)$/i.exec(path)) !== null) {
+    // https://sahkoverkkoekstra.fi/filedepot_download/2576/403
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = match[1] + '/' + match[2];
+  } else if ((match = /^\/([0-9a-z_-]+)\/([a-z0-9-A-Z-_/]*)$/i.exec(path)) !== null && match[1] !== 'file' && match[1] !== 'filedepot_download' && match[1] !== 'system' && match[1] !== 'search') {
+    // https://sahkoverkkoekstra.fi/verkkotoiminnan-valvonta/valvontamenetelmien-suuntaviivat/suuntaviivojen-ensimmainen-versio
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid   = match[1] + '/' + match[2];
+  } else if ((match = /^\/search\/site\/(.*)$/i.exec(path))) {
+    // https://sahkoverkkoekstra.fi:443/search/site/muuntamo suojaus
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+    result.search_term   = match[1];
   }
+
+
 
   return result;
 });
