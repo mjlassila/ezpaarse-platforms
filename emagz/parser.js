f45c116fc110f42d429bbd41b8384c9142647a23
Matti Lassila <matti.lassila@gmail.com>
2022-01-26 15:18:50 +0200

    Simple parser for eMagz. Currently handles only Tutkiva hoitotiede -journal

--- emagz/parser.js
+++ emagz/parser.js
@@ -5,6 +5,7 @@ const Parser = require('../.lib/parser.js');
 
 /**
  * Recognizes the accesses to the platform eMagz
+ * Currently used only for Tutkiva Hoitotyö, for which proxy usage is allowed.
  * @param  {Object} parsedUrl an object representing the URL to analyze
  *                            main attributes: pathname, query, hostname
  * @param  {Object} ec        an object representing the EC whose URL is being analyzed
@@ -14,33 +15,24 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+  if ((/^\/read_issue$/i.exec(path)) !== null) {
+    // https://tuhto.emagz.fi/read_issue?issue_id=272699&title_id=10228
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = match[1];
+    result.title_id = param.title_id;
+    result.unitid = param.issue_id;
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+  } else if ((/^\/archive$/i.exec(path)) !== null) {
+    // https://tuhto.emagz.fi/archive?title_id=10228
+    result.rtype    = 'TOC';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid = param.title_id;
   }
 
   return result;
