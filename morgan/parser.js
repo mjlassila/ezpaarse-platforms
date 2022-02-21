014e0518c47b39f7c2e0657d55c44ee03c7c22d9
Matti Lassila <matti.lassila@gmail.com>
2022-02-21 11:47:48 +0200

    Initial parser for Morgan & Claypool

--- morgan/parser.js
+++ morgan/parser.js
@@ -14,18 +14,19 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+  if ((match = /^\/doi\/(?:pdf|pdfplus)\/(10.4199.*|10.2200.*)$/i.exec(path)) !== null) {
+    // https://www-morganclaypool-com.libproxy.tuni.fi/doi/pdf/10.2200/S0113ED1V01Y202109ICR076
+    // https://www-morganclaypool-com.libproxy.tuni.fi/doi/pdfplus/10.2200/S0113ED1V01Y202109ICR076
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = match[1];
+    result.unitid = match[1];
 
     /**
      * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
@@ -33,14 +34,34 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
      * it can be a DOI, an internal identifier or a part of the accessed URL
      * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
      */
-    result.unitid = match[2];
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+  } else if ((match = /^\/doi\/(?:abs\/)?(10.4199.*|10.2200.*)$/i.exec(path)) !== null) {
+    // https://www.morganclaypool.com/doi/10.2200/S00723ED1V01Y201607ICR050
+    // https://www.morganclaypool.com/doi/abs/10.2200/S00723ED1V01Y201607ICR050
+    result.rtype    = 'ABSTRACT';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid = match[1];
+  } else if ((match = /^\/toc\/([a-z]+)\/1\/1$/i.exec(path)) !== null) {
+    // https://www.morganclaypool.com//toc/can/1/1
+    result.rtype    = 'TOC';
+    result.mime = 'HTML';
+    result.unitid   = match[1];
+  } else if ((match = /^\/author\/(.*)$/i.exec(path)) !== null) {
+    // https://www.morganclaypool.com/author/Weiner%2C+Joyce
+    result.rtype    = 'SEARCH';
+    result.mime = 'HTML';
+    result.search_term   = match[1];
+  } else if ((match = /^\/action\/doSearch/i.exec(path)) !== null) {
+    // https://www.morganclaypool.com/action/doSearch?AllField=information+retrieval&x=12&y=14&SeriesKey=
+    result.rtype    = 'SEARCH';
+    result.mime = 'HTML';
+    // Capture search term only for simple search
+    result.search_term   = param.AllField;
+  } else if ((match = /^\/doi\/suppl\/(10.4199.*|10.2200.*)\/(.*)/i.exec(path)) !== null) {
+    // https://www.morganclaypool.com/doi/suppl/10.2200/S00484ED1V01Y201303PRO001/suppl_file/Winner_Ch1.pdf
+    result.rtype    = 'SUPPL';
+    result.mime = 'PDF';
+    result.unitid   = match[1];
   }
 
   return result;
