2bd96c45bc028d2677204020307c3f93f5ac2933
Matti Lassila <matti.lassila@gmail.com>
2022-02-28 16:46:21 +0200

    Parser for KnowPap learning platform

--- knowpap/parser.js
+++ knowpap/parser.js
@@ -19,29 +19,13 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+  if ((/^\/extranet\/([a-z]+)\/knowpap_system\/user_interfaces\/knowpap\.htm$/i.exec(path)) !== null) {
+    // http://www.knowpap.com/extranet/suomi/knowpap_system/user_interfaces/knowpap.htm
+    result.rtype    = 'OTHER';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid = 'knowpap';
   }
-
   return result;
 });
